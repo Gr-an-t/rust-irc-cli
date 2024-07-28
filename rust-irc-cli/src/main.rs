0192d33e8806f7799de49d259f4b75179713933d
DrVario <grantporter343@gmail.com>
2024-07-28 01:39:07 -0500

    Added threading pools, Added CLI params

--- rust-irc-cli/src/main.rs
+++ rust-irc-cli/src/main.rs
@@ -1,10 +1,49 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
+use std::{
+    io::{
+        prelude::*,
+        BufReader,
+    },
+    net::{
+        TcpListener, 
+        TcpStream,
+    },
+};
+use threadpool::ThreadPool;
+use clap::Parser;
 
-fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
+#[derive(Parser)]
+#[clap(name = "rust-irc-cli", version = "0.1.0", author = "Gr-an-t")]
+struct Cli {
+    #[arg(short, long, default_value = "127.0.0.1")]
+    a: String,
 
-    stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
-    Ok(())
+    #[arg(short, long, default_value = "7878")]
+    p: u16,
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines().map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+
+    println!("Request: {:?}", http_request);
 }
